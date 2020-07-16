954f14ed224bfa44296f607cb740b164b9b5a9a5
Jonathan Forby <jonathan@forby.co>
2020-07-16 09:23:28 -0400

    purge unused styles on build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: [],
+    purge: [
+        "./src/**/*.js"
+    ],
     theme: {
         extend: {
             fontFamily: {
